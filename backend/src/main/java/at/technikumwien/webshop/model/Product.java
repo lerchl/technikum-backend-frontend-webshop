8b117a014034e11bd84d89783835fd7043424024
Nico Lerchl <nico@lerchl.one>
2023-01-26 09:54:42 +0100

    backend database connection

--- backend/src/main/java/at/technikumwien/webshop/model/Product.java
+++ backend/src/main/java/at/technikumwien/webshop/model/Product.java
@@ -1,14 +1,39 @@
 package at.technikumwien.webshop.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
 public class Product {
     
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private final Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "image_url")
     private String imageUrl;
+
+    @Column(name = "price")
     private double price;
+
+    @Column(name = "quantity")
     private int quantity;
+
+    @Column(name = "type")
     private String type;
+
+    // /////////////////////////////////////////////////////////////////////////
+    // Init
+    // /////////////////////////////////////////////////////////////////////////
 
     public Product(Long id, String name, String description, String imageUrl, double price, int quantity, String type) {
         this.id = id;
@@ -20,6 +45,12 @@ public class Product {
         this.type = type;
     }
 
+    // /////////////////////////////////////////////////////////////////////////
+    // Getters and Setters
+    // /////////////////////////////////////////////////////////////////////////
+
+    
+
     public Long getId() {
         return id;
     }
@@ -28,23 +59,47 @@ public class Product {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getImageUrl() {
         return imageUrl;
     }
 
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public String getType() {
         return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
