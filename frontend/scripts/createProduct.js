e00228a956e7a6c0df06eff4472f14fe8c5bcf8c
Nico Lerchl <nico@lerchl.one>
2023-02-10 14:14:09 +0100

    color wrong inputs and add error infos

--- frontend/scripts/createProduct.js
+++ frontend/scripts/createProduct.js
@@ -21,6 +21,13 @@ function addTaxRatesToSelect(taxRates) {
 // hier wird dem Element mit der Id "createProductButton" ein listener
 // für das onclick-Event hinzugefügt
 $("#createProductButton").on("click", _e => {
+    // entfernen der input-error class von allen elementen,
+    // die die input-error class besitzen
+    $(".input-error").removeClass("input-error");
+
+    // löscht alle elemente mit der klasse error-message
+    $(".error-message").remove();
+
     // wir erstellen ein object und setzen die 
     // Werte aus den Eingabefeldern
     const product = {
@@ -43,7 +50,23 @@ $("#createProductButton").on("click", _e => {
         data: JSON.stringify(product),
         success: console.log,
         error: error => {
+            console.log(error);
             if (error.status === 400) {
+
+                // über alle Fehler iterieren
+                for (let err of error.responseJSON.errors) {
+                    // das inputfeld entsprechend dem Fehler raussuchen
+                    const input = $("#" + err.field + "Input");
+                    // klasse für die anzeige des fehlerhaften feldes hinzufügen
+                    input.addClass("input-error");
+
+                    // den parent des inputfeldes raussuchen
+                    const parent = input.parent();
+                    // anfügen der fehlermeldung im parent
+                    parent.append(`<p class="error-message">${err.defaultMessage}</p>`);
+                }
+
+                // aufruf der Funktion, damit die Toasts angezeigt werden
                 handleErrors(error.responseJSON.errors);
             }
         }
